5781ddc749283334df681a28e59c5785520ee153
Hamza Mezerreg <hamza.mez@gmail.com>
2025-09-13 22:02:46 +0100

    echo4 : prints its command-line arguments.

---
+++ ch2/echo/echo.go
@@ -0,0 +1,19 @@
+// Echo4 prints its command-line arguments.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("sep", " ", "separator")
+
+func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
